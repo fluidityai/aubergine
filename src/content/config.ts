d1d9ba89cd4fdaedadbfc6e5cd9d93049ffcfd61
Michael Andreuzza <Michaelandreuzzavega@gmail.com>
2024-03-10 17:50:55 +0200

    added new pages - content collections - new nav

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,58 @@
 import { defineCollection, z } from "astro:content";
-
+const team = defineCollection({
+  schema: z.object({
+    name: z.string(),
+    role: z.string(),
+    intro: z.string(),
+    education: z.array(z.string()),
+    experience: z.array(z.string()),
+    avatar: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const services = defineCollection({
+  schema: z.object({
+    service: z.string(),
+    description: z.string(),
+  }),
+});
+const work = defineCollection({
+  schema: z.object({
+    launchDate: z.date(),
+    client: z.string(),
+    work: z.string(),
+    description: z.string(),
+    logo: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+const infopages = defineCollection({
+  schema: z.object({
+    page: z.string(),
+    pubDate: z.date(),
+  }),
+});
 const postsCollection = defineCollection({
-    schema: z.object({
-      title: z.string(),
-      pubDate: z.date(),
-      description: z.string(),
-      author: z.string(),
-
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-     
-      tags: z.array(z.string())
-    })
- });
-
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
 export const collections = {
+  team: team,
+  services: services,
+  work: work,
+  infopages: infopages,
   posts: postsCollection,
 };
---
+++ src/content/infopages/about.md
@@ -0,0 +1,14 @@
+---
+page: About
+pubDate: 2024-01-01
+---
+
+Founders turn to Aubergine when success is non-negotiable. Our reputation for design expertise and swift execution precedes us, drawing clients who prioritize excellence. Yet, more often than not, it's the word-of-mouth referrals from trusted sources – be it an investor, a friend, or a connection on Twitter/X – that bring clients to Aubergine.
+
+With over a decade of experience and the launch of numerous successful projects, our work is likely familiar to you. Startups, enterprises, conglomerates, and government entities alike have engaged Aubergine to chart their strategic next steps.
+
+Throughout our journey, we've honed a distinctive work approach that prioritizes agility, allowing us to navigate projects at an unparalleled pace. It's our secret sauce, but you can experience it firsthand by partnering with us.
+Our pricing structure begins at $50,000/month.
+
+
+/Aubergine Theme
