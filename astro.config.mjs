70a2e6ac39c6f758f4ce855ac29565d3053992fd
Jonathan Seneris <3938551+jonathanseneris@users.noreply.github.com>
2024-05-27 01:00:15 -0400

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 export default defineConfig({
   site: "https://fluidityai.github.io",
-  // base: 'aubergine',
+  base: 'aubergine',
   markdown: {
     drafts: true,
     shikiConfig: {
