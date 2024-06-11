423cc951852d84e1f758cb14c209df82e10160c8
Jonathan Seneris <3938551+jonathanseneris@users.noreply.github.com>
2024-06-11 04:41:57 -0400

    marketing updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import robots from "astro-robots";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://fluidityai.github.io",
   markdown: {
     drafts: true,
     shikiConfig: {
-      theme: "css-variables",
-    },
+      theme: "css-variables"
+    }
   },
   shikiConfig: {
     wrap: true,
     skipInline: false,
-    drafts: true,
+    drafts: true
   },
-  integrations: [tailwind(), sitemap(), mdx()],
+  integrations: [tailwind(), sitemap(), mdx(), robots()]
 });
